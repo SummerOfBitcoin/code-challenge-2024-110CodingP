cac91f6abb1fc0abb27c259368c3260e2c90a6b3
Garima Chauhan <codingp110@gmail.com>
2024-03-28 11:10:07 +0530

    write output to required .txt file

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("0000000000000000000000000000000000000000000000000000000000000000");
+const fs = require('node:fs');
+const content = '000000000000000000000000000000000000000000000000000000000000000'
+try {
+  fs.writeFileSync('output.txt', content, {flag: 'a+'});
+}
+catch(err){
+  console.log(err);
+}
