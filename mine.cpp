41ad603ffca1bb94fd53b5f99f2790fa89fe76b2
Garima Chauhan <codingp110@gmail.com>
2024-04-07 22:53:47 +0530

    changed language to c++

---
+++ mine.cpp
@@ -0,0 +1,11 @@
+#include <iostream>//damn! the steam is really I/O stream!
+using namespace std;
+
+//create a serialise function to serialise txns
+
+int main() {
+    //use freopen etc. to read json files
+    freopen("output.txt","w",stdout);
+    cout<<"Hello World\n";
+    return 0;
+}
