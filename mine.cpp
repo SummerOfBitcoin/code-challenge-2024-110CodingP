5bfbcc91a246526fce7c4b5b2e7ba67d5ffd7845
Garima Chauhan <codingp110@gmail.com>
2024-04-26 17:41:59 +0530

    learnt to hash in cpp

--- mine.cpp
+++ mine.cpp
@@ -4,7 +4,36 @@
 #include <vector>
 using namespace std;
 
-//create a serialise function to serialise txns
+
+struct Output {
+  int Amount;
+  string ScriptPubKey;
+}
+
+struct Input {
+  string txId;
+  int vOut;
+  string scriptSig;
+  string sequence;
+  vector<string> witness;
+  vector<Output> prevout;
+  string scriptSigAsm;
+  bool isCoinbase;
+}
+
+struct Txn {
+  int version;
+  int locktime;
+  vector<Input> vin;
+  vector<Output> vout;
+
+}
+
+string serialise(string content) {
+    string serialised;
+    int sz = content.size();
+    return serialised;
+}
 
 int main() {
 
@@ -12,21 +41,27 @@ int main() {
 
     ifstream myfile;
     myfile.open("txns.txt");
-    string mystring;
- 
+    string filename;
     vector<string> txns;
 
     while (myfile.good()) {
-        getline(myfile,mystring);
-        txns.push_back(mystring);
+        getline(myfile,filename);
+        txns.push_back(filename);
     }
     myfile.close();
 
-    for (string txn: txns) {
-      string filename = "mempool/" + txn;
-      myfile.open(filename);
-      //getline(myfile,mystring);
-      //cout<<mystring<<'\n';
+    for (string txn: txns) {//man this outer loop is running approx 8000 times!
+      string path = "mempool/" + txn;
+      myfile.open(path);
+      string content;
+      while (myfile.good()) {
+        string line;
+        getline(myfile,line);
+        content+=line;
+      }
+      serialise(content);
+
+      myfile.close();
     }
     return 0;
 }
