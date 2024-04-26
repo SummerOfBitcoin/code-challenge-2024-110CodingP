628693ad04ca613d35173e84099b001d24395568
Garima Chauhan <codingp110@gmail.com>
2024-04-26 23:58:08 +0530

    try using nlohmann-json to parse json data

---
+++ trying_nlohman_json.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <json.hpp>
+#include <fstream>
+
+using json = nlohmann::json;
+
+struct Test {
+   std::string Name;
+   std::string Val;
+};
+
+ void from_json(const json& j, Test& p) {
+    j.at("Name").get_to(p.Name);
+    j.at("Val").get_to(p.Val);
+}
+
+template <typename T, size_t N>
+void from_json(const json& j, T  (&t)[N]) {
+    if (j.size()!=N) {
+        throw std::runtime_error("size!");
+    }
+    size_t index=0;
+    for (auto& item : j) {
+        from_json(item, t[index++]);
+    }
+}
+int main() {
+    std::ifstream f("test.json");
+    json data = json::parse(f);
+    Test my_array[2];
+    from_json(data, my_array);
+    std::cout<<my_array[0].Name<<std::endl;
+    return 0;
+}
