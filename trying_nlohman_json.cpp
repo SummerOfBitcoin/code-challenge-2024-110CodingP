43123be47aed459b4c10f304a951b4c96b1166ab
Garima Chauhan <codingp110@gmail.com>
2024-04-27 11:04:31 +0530

    parse transaction data using nlohmann-json library

--- trying_nlohman_json.cpp
+++ trying_nlohman_json.cpp
@@ -16,16 +16,16 @@ struct Test {
     j.at("Val").get_to(p.Val);
 }
 
-template <typename T, size_t N>
-void from_json(const json& j, T  (&t)[N]) {
-    if (j.size()!=N) {
-        throw std::runtime_error("size!");
-    }
-    size_t index=0;
-    for (auto& item : j) {
-        from_json(item, t[index++]);
-    }
-}
+// template <typename T, size_t N>
+// void from_json(const json& j, T  (&t)[N]) {
+//     if (j.size()!=N) {
+//         throw std::runtime_error("size!");
+//     }
+//     size_t index=0;
+//     for (auto& item : j) {
+//         from_json(item, t[index++]);
+//     }
+// }
 int main() {
     std::ifstream f("test.json");
     json data = json::parse(f);
@@ -34,3 +34,14 @@ int main() {
     std::cout<<my_array[0].Name<<std::endl;
     return 0;
 }
+
+// [
+//     {
+//       "Name": "Test",
+//       "Val": "TestVal"
+//     },
+//     {
+//       "Name": "Test2",
+//       "Val": "TestVal2"
+//     }
+// ]
